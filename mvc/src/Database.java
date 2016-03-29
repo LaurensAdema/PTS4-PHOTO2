4d8b5b8413a13511c4bf2c90d297e216280cf317
LaurensAdema <laurensadema@live.nl>
2016-03-29 10:07:30 +0200

    New database

--- mvc/src/Database.java
+++ mvc/src/Database.java
@@ -1,4 +1,3 @@
-
 import java.sql.*;
 
 /*
@@ -6,49 +5,55 @@ import java.sql.*;
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 /**
  *
  * @author soufyan
  */
 public class Database {
-    public static void main(String[] args) throws SQLException {
 
-		Connection myConn = null;
-		Statement myStmt = null;
-		ResultSet myRs = null;
-		
-		try {
-			// 1. Get a connection to database
-			myConn = DriverManager.getConnection("jdbc:mysql://localhost:3306/photoshop", "root","megamen12");
-			
-			System.out.println("Database connection successful!\n");
-			
-			// 2. Create a statement
-			myStmt = myConn.createStatement();
-			
-			// 3. Execute SQL query
-			myRs = myStmt.executeQuery("select * from account");
-			
-			// 4. Process the result set
-			while (myRs.next()) {
-				System.out.println(myRs.getString("username") + ", ");
-			}
-		}
-		catch (Exception exc) {
-			exc.printStackTrace();
-		}
-		finally {
-			if (myRs != null) {
-				myRs.close();
-			}
-			
-			if (myStmt != null) {
-				myStmt.close();
-			}
-			
-			if (myConn != null) {
-				myConn.close();
-			}
-		}
+    public static void main(String[] args) throws SQLException
+    {
+        Connection myConn = null;
+        Statement myStmt = null;
+        ResultSet myRs = null;
+
+        try
+        {
+            // 1. Get a connection to database
+            myConn = DriverManager.getConnection("jdbc:mysql://www.ict4events.nl:3306/photoshop", "photoshop", "pts4");
+
+            System.out.println("Database connection successful!\n");
+
+            // 2. Create a statement
+            myStmt = myConn.createStatement();
+
+            // 3. Execute SQL query
+            myRs = myStmt.executeQuery("select * from account");
+
+            // 4. Process the result set
+            while (myRs.next())
+            {
+                System.out.println(myRs.getString("username") + ", ");
+            }
+        } catch (Exception exc)
+        {
+            exc.printStackTrace();
+        } finally
+        {
+            if (myRs != null)
+            {
+                myRs.close();
+            }
+
+            if (myStmt != null)
+            {
+                myStmt.close();
+            }
+
+            if (myConn != null)
+            {
+                myConn.close();
+            }
+        }
+    }
 }

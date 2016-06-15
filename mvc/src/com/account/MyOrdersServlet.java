bd5014fd7b0ed4e6ac3e5587588bd193de91260c
NesciO <mayflair@live.nl>
2016-06-15 04:40:18 +0200

    niet zelf de url veranderen laurens

--- mvc/src/com/account/MyOrdersServlet.java
+++ mvc/src/com/account/MyOrdersServlet.java
@@ -43,43 +43,44 @@ public class MyOrdersServlet extends HttpServlet {
         Bestelling order = null;
         Account account = (Account) request.getSession().getAttribute("account");
         int accountID = -1;
+        if (request.getParameter("id") != null && account != null) {
+            try {
 
-        try {
+                ResultSet results = Database.getDatabase().query("SELECT * FROM ordera WHERE id = " + request.getParameter("id") + " AND accountID = " + account.getID(), Database.QUERY.QUERY);
 
-            ResultSet results = Database.getDatabase().query("SELECT * FROM ordera WHERE id = " + request.getParameter("id"), Database.QUERY.QUERY);
+                if (results != null) {
+                    while (results.next()) {
+                        order = new Bestelling(results.getInt("price"), results.getDate("date"), results.getInt("id"));
+                        accountID = results.getInt("accountID");
+                    }
+                    ResultSet results1 = Database.getDatabase().query("SELECT product.*, order_product.quantity FROM product, order_product WHERE id IN ( SELECT productID FROM order_product WHERE orderID = " + order.getId() + " ) AND product.id=order_product.productID", Database.QUERY.QUERY);
 
-            if (results != null) {
-                while (results.next()) {
-                    order = new Bestelling(results.getInt("price"), results.getDate("date"), results.getInt("id"));
-                    accountID = results.getInt("accountID");
-                }
-                ResultSet results1 = Database.getDatabase().query("SELECT product.*, order_product.quantity FROM product, order_product WHERE id IN ( SELECT productID FROM order_product WHERE orderID = " + order.getId() + " ) AND product.id=order_product.productID", Database.QUERY.QUERY);
-
-                if (results1 != null) {
-                    while (results1.next()) {
-                        Product product = new Product(results1.getInt("id"), results1.getString("material"), results1.getString("color"), results1.getString("size"), null, results1.getInt("price"));
-                        order.getProducts().put(product, results1.getInt("quantity"));
-                        Photo photo;
-                        ResultSet results2 = Database.getDatabase().query("SELECT * FROM photo WHERE id = " + results1.getInt("photoID"), Database.QUERY.QUERY);
-                        if (results2 != null) {
-                            while (results2.next()) {
-                                photo = new Photo(results2.getString("name"), results2.getInt("price"), results2.getInt("id"), results2.getString("pathlowres"), results2.getString("pathhighres"));
-                                product.setPhoto(photo);
-                                product.setName(photo.getName());
-                                order.getProducts().put(product, order.getProducts().get(product));
+                    if (results1 != null) {
+                        while (results1.next()) {
+                            Product product = new Product(results1.getInt("id"), results1.getString("material"), results1.getString("color"), results1.getString("size"), null, results1.getInt("price"));
+                            order.getProducts().put(product, results1.getInt("quantity"));
+                            Photo photo;
+                            ResultSet results2 = Database.getDatabase().query("SELECT * FROM photo WHERE id = " + results1.getInt("photoID"), Database.QUERY.QUERY);
+                            if (results2 != null) {
+                                while (results2.next()) {
+                                    photo = new Photo(results2.getString("name"), results2.getInt("price"), results2.getInt("id"), results2.getString("pathlowres"), results2.getString("pathhighres"));
+                                    product.setPhoto(photo);
+                                    product.setName(photo.getName());
+                                    order.getProducts().put(product, order.getProducts().get(product));
+                                }
                             }
                         }
                     }
                 }
-            }
 
-        } catch (SQLException ex) {
-            Logger.getLogger(MyOrdersServlet.class.getName()).log(Level.SEVERE, null, ex);
-        } finally {
-            Database.getDatabase().closeConnection();
-        }
-        if (accountID == account.getID()) {
-            request.getSession().setAttribute("order", order);
+            } catch (SQLException ex) {
+                Logger.getLogger(MyOrdersServlet.class.getName()).log(Level.SEVERE, null, ex);
+            } finally {
+                Database.getDatabase().closeConnection();
+            }
+            if (accountID == account.getID()) {
+                request.getSession().setAttribute("order", order);
+            }
         }
 
     }
